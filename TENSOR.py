3f5fe4dc40c7bc5c8da078464e53d092c46a47f3
lazarod09 <lazarod09@fmail.com>
2020-12-26 16:25:56 -0500

    Primera version

--- TENSOR.py
+++ TENSOR.py
@@ -1,112 +1,86 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow
-yc = 0
-r2 = 0
+
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+import tensorflow_model_optimization as tfmot
+from tensorflow import keras
+import tempfile
+from sklearn.model_selection import train_test_split
+
 data = pd.read_csv(r'yeni.csv')
 X = data[['X1', 'X2', 'X3', 'X4', 'X5']].values
 Y = data['Y'].values
 
-
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 
-def sigmoid_derivada(x):
-    return sigmoid(x) * (1.0 - sigmoid(x))
+#Define the model architecture.
+def setup_model():
+    model = keras.Sequential([
+      keras.layers.InputLayer(input_shape=(28, 28)),
+      keras.layers.Reshape(target_shape=(28, 28, 1)),
+      keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation='relu'),
+      keras.layers.MaxPooling2D(pool_size=(2, 2)),
+      keras.layers.Flatten(),
+      keras.layers.Dense(10)
+    ])
+    return model
 
+# Train the digit classification model
+def setup_pretrained_weights():
+  model= setup_model()
 
-def tanh(x):
-    return np.tanh(x)
+  model.compile(
+      loss=tf.keras.losses.categorical_crossentropy,
+      optimizer='adam',
+      metrics=['accuracy']
+  )
 
+  model.fit(X_train,y_train)
 
-def tanh_derivada(x):
-    return 1.0 - x ** 2
+  _, pretrained_weights = tempfile.mkstemp('.tf')
 
+  model.save_weights(pretrained_weights)
 
-class NeuralNetwork:
+  return pretrained_weights
 
-    def __init__(self, layers, activation='tanh'):
-        if activation == 'sigmoid':
-            self.activation = sigmoid
-            self.activation_prime = sigmoid_derivada
-        elif activation == 'tanh':
-            self.activation = tanh
-            self.activation_prime = tanh_derivada
+def setup_pretrained_model():
+  model = setup_model()
+  pretrained_weights = setup_pretrained_weights()
+  model.load_weights(pretrained_weights)
+  return model
 
-        # inicializo los pesos
-        self.weights = []
-        self.deltas = []
-        # capas = [2,3,2]
-        # rando de pesos varia entre (-1,1)
-        # asigno valores aleatorios a capa de entrada y capa oculta
-        for i in range(1, len(layers) - 1):
-            r = 2 * np.random.random((layers[i - 1] + 1, layers[i] + 1)) - 1
-            self.weights.append(r)
-        # asigno aleatorios a capa de salida
-        r = 2 * np.random.random((layers[i] + 1, layers[i + 1])) - 1
-        self.weights.append(r)
+setup_model()
+pretrained_weights = setup_pretrained_weights()
 
-    def fit(self, X, y, learning_rate=0.2, epochs=100000):
-        # Agrego columna de unos a las entradas X
-        # Con esto agregamos la unidad de Bias a la capa de entrada
-        ones = np.atleast_2d(np.ones(X.shape[0]))
-        X = np.concatenate((ones.T, X), axis=1)
+base_model = setup_model()
+base_model.load_weights(pretrained_weights) # optional but recommended for model accuracy
 
-        for k in range(epochs):
-            i = np.random.randint(X.shape[0])
-            a = [X[i]]
+quant_aware_model = tfmot.quantization.keras.quantize_model(base_model)
+quant_aware_model.summary()
+# Create a base model
+base_model = setup_model()
+base_model.load_weights(pretrained_weights) # optional but recommended for model accuracy
 
-            for l in range(len(self.weights)):
-                dot_value = np.dot(a[l], self.weights[l])
-                activation = self.activation(dot_value)
-                a.append(activation)
-            # Calculo la diferencia en la capa de salida y el valor obtenido
-            error = y[i] - a[-1]
-            deltas = [error * self.activation_prime(a[-1])]
+# Helper function uses `quantize_annotate_layer` to annotate that only the
+# Dense layers should be quantized.
+def apply_quantization_to_dense(layer):
+  if isinstance(layer, tf.keras.layers.Dense):
+    return tfmot.quantization.keras.quantize_annotate_layer(layer)
+  return layer
 
-            # Empezamos en el segundo layer hasta el ultimo
-            # (Una capa anterior a la de salida)
-            for l in range(len(a) - 2, 0, -1):
-                deltas.append(deltas[-1].dot(self.weights[l].T) * self.activation_prime(a[l]))
-            self.deltas.append(deltas)
+# Use `tf.keras.models.clone_model` to apply `apply_quantization_to_dense`
+# to the layers of the model.
+annotated_model = tf.keras.models.clone_model(
+    base_model,
+    clone_function=apply_quantization_to_dense,
+)
 
-            # invertir
-            # [level3(output)->level2(hidden)]  => [level2(hidden)->level3(output)]
-            deltas.reverse()
-
-            # backpropagation
-            # 1. Multiplcar los delta de salida con las activaciones de entrada
-            #    para obtener el gradiente del peso.
-            # 2. actualizo el peso restandole un porcentaje del gradiente
-            for i in range(len(self.weights)):
-                layer = np.atleast_2d(a[i])
-                delta = np.atleast_2d(deltas[i])
-                self.weights[i] += learning_rate * layer.T.dot(delta)
-
-            if k % 10000 == 0: print('epochs:', k)
-
-    def predict(self, x):
-        ones = np.atleast_2d(np.ones(x.shape[0]))
-        a = np.concatenate((np.ones(1).T, np.array(x)), axis=0)
-        for l in range(0, len(self.weights)):
-            a = self.activation(np.dot(a, self.weights[l]))
-        return a
-
-    def print_weights(self):
-        print("LISTADO PESOS DE CONEXIONES")
-        for i in range(len(self.weights)):
-            print(self.weights[i])
-
-    def get_deltas(self):
-        return self.deltas
-
-
-nn = NeuralNetwork([2, 3, 2], activation='tanh')
-nn.fit(X, Y, learning_rate=0.03, epochs=15001)
-
-index = 0
-for e in X:
-    print("X:", e, "Y:", y[index], "Network:", nn.predict(e))
-    index = index + 1
+# Now that the Dense layers are annotated,
+# `quantize_apply` actually makes the model quantization aware.
+quant_aware_model = tfmot.quantization.keras.quantize_apply(annotated_model)
+quant_aware_model.summary()
+print(base_model.layers[0].name)
